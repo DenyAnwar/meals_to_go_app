920b80819acbe2ae7a427c985517f77fc5f6415a
Deny Anwar <denyanwardev@gmail.com>
2023-12-27 06:14:19 +0800

    add restaurant card

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Searchbar } from "react-native-paper";
+import { StatusBar, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+export const RestaurantScreen = () => (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+      <Searchbar />
+    </View>
+    <View style={styles.list}>
+      <RestaurantInfoCard />
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
