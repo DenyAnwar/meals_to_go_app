00188d6e6559ade7ea9f4cb128613ee149cfef32
denyanwardev <denyanwardev@gmail.com>
2024-06-25 17:19:42 +0800

    trying out styled components

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import {
-  StatusBar,
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  View,
-  Platform,
-} from "react-native";
+import React from "react";
+
+import { RestaurantScreen } from "./src/components/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>Searchx</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({});
