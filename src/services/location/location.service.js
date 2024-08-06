668809dd07361de2439482a1d9c96659a458f875
deny_anwar <denyanwardev@gmail.com>
2024-08-06 17:32:11 +0800

    moving location mock

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,17 +1,15 @@
 import camelize from "camelize";
-import { locations } from "./location.mock";
 
 export const locationRequest = (searchTerm) => {
-  return new Promise((resolve, reject) => {
-    const locationMock = locations[searchTerm];
-    if (!locationMock) {
-      reject("not found");
-    }
-    resolve(locationMock);
+  return fetch(
+    `https://2a5e-114-122-230-43.ngrok-free.app/mealstogo-beadd/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
 export const locationTransform = (result) => {
+  console.log(result);
   const formattedResponse = camelize(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
